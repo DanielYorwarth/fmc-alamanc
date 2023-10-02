773edbc2cf3ebdca157b0741fb2c94945e07a0b6
PageCreativeStudio <web@pagecreative.co.uk>
2023-10-02 17:05:41 +0100

    Updated files

--- src/components/events-timeline-horizontal/event-timeline-horizontal.component.jsx
+++ src/components/events-timeline-horizontal/event-timeline-horizontal.component.jsx
@@ -3,14 +3,15 @@ import { Box, Flex } from "reflexbox"
 import { Circle, Line, Title, Wrapper, Text, DateWrapper } from "./event-timeline-horizontal.styles"
 import { withTheme } from "@emotion/react"
 
-const EventTimelineHorizontal = ({theme, events, title}) => {
+const EventTimelineHorizontal = ({theme, event, title}) => {
   return (
     <Box>
       {title && <Box marginBottom={theme.spacing[1]}>
         <Title>{title}</Title>
       </Box>}
       <Wrapper>
-        {events && events.map(({date, time, title, colour=theme.colour.primary}) => <DateWrapper flexDirection="column" key={title} minWidth="20rem" width="24rem" marginBottom={theme.spacing[1]} marginRight={theme.spacing[1]}>
+        {event && event.map(({date, time, title, colour=theme.colour.primary}) => 
+        <DateWrapper flexDirection="column" key={title} minWidth="20rem" width="24rem" marginBottom={theme.spacing[1]} marginRight={theme.spacing[1]}>
           <Box maxWidth="24rem" marginBottom={theme.spacing[0]}>
             <Circle colour={colour} />
           </Box>
