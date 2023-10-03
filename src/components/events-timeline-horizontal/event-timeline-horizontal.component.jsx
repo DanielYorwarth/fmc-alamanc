5a53a4f259d9b3f76c3f7f7a20805e47293f7af9
PageCreativeStudio <web@pagecreative.co.uk>
2023-10-03 09:37:01 +0100

    Updated files

--- src/components/events-timeline-horizontal/event-timeline-horizontal.component.jsx
+++ src/components/events-timeline-horizontal/event-timeline-horizontal.component.jsx
@@ -1,29 +1,44 @@
-import React from "react"
-import { Box, Flex } from "reflexbox"
-import { Circle, Line, Title, Wrapper, Text, DateWrapper } from "./event-timeline-horizontal.styles"
-import { withTheme } from "@emotion/react"
+import React from "react";
+import { Box } from "reflexbox";
+import { Circle, Line, Title, Wrapper, Text, DateWrapper } from "./event-timeline-horizontal.styles";
+import { withTheme } from "@emotion/react";
 
-const EventTimelineHorizontal = ({theme, event, title}) => {
+const EventTimelineHorizontal = ({ theme, events, title }) => {
   return (
     <Box>
-      {title && <Box marginBottom={theme.spacing[1]}>
-        <Title>{title}</Title>
-      </Box>}
+      {title && (
+        <Box marginBottom={theme.spacing[1]}>
+          <Title>{title}</Title>
+        </Box>
+      )}
       <Wrapper>
-        {event && event.map(({date, time, title, colour=theme.colour.primary}) => 
-        <DateWrapper flexDirection="column" key={title} minWidth="20rem" width="24rem" marginBottom={theme.spacing[1]} marginRight={theme.spacing[1]}>
-          <Box maxWidth="24rem" marginBottom={theme.spacing[0]}>
-            <Circle colour={colour} />
-          </Box>
-          <Box>
-            {(date || time) && <Text>{date && date}{time && <>, {time}</>}</Text>}
-            {title && <Title>{title}</Title>}
-          </Box>
-        </DateWrapper>)}
+        {events && events.map((event) => (
+          <DateWrapper
+            flexDirection="column"
+            key={event.id}
+            minWidth="20rem"
+            width="24rem"
+            marginBottom={theme.spacing[1]}
+            marginRight={theme.spacing[1]}
+          >
+            <Box maxWidth="24rem" marginBottom={theme.spacing[0]}>
+              <Circle colour={event.color} />
+            </Box>
+            <Box>
+              {(event.start || event.time) && (
+                <Text>
+                  {event.start && event.start}
+                  {event.time && <>, {event.time}</>}
+                </Text>
+              )}
+              {event.name && <Title>{event.name}</Title>}
+            </Box>
+          </DateWrapper>
+        ))}
         <Line />
       </Wrapper>
     </Box>
-  )
-}
+  );
+};
 
-export default withTheme(EventTimelineHorizontal)
+export default withTheme(EventTimelineHorizontal);
