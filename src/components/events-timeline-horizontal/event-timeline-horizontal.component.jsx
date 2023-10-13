00fd8e292aac2c4e88d30966beb20866bd306859
PageCreativeStudio <web@pagecreative.co.uk>
2023-10-13 10:29:42 +0100

    Updated files

--- src/components/events-timeline-horizontal/event-timeline-horizontal.component.jsx
+++ src/components/events-timeline-horizontal/event-timeline-horizontal.component.jsx
@@ -4,6 +4,34 @@ import { Circle, Line, Title, Wrapper, Text, DateWrapper } from "./event-timelin
 import { withTheme } from "@emotion/react";
 
 const EventTimelineHorizontal = ({ theme, events, title }) => {
+  // Function to format the date in British English
+  const formatDate = (dateStr) => {
+    const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    };
+    const date = new Date(dateStr);
+    return date.toLocaleDateString("en-GB", options);
+  };
+
+  // Function to format the time
+  const formatTime = (timeStr) => {
+    // Check if the time is valid
+    const time = new Date(`2000-01-01T${timeStr}`);
+    if (isNaN(time)) {
+      return timeStr; // Use the original time if it's not valid
+    }
+
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+    return time.toLocaleTimeString("en-GB", options);
+  };
+
   return (
     <Box>
       {title && (
@@ -27,8 +55,10 @@ const EventTimelineHorizontal = ({ theme, events, title }) => {
             <Box>
               {(event.start || event.time) && (
                 <Text>
-                  {event.start && event.start}
-                  {event.time && <>, {event.time}</>}
+                  {event.start && formatDate(event.start)}
+                  {event.start && event.time && (
+                    `, ${formatTime(event.time)}`
+                  )}
                 </Text>
               )}
               {event.name && <Title>{event.name}</Title>}
